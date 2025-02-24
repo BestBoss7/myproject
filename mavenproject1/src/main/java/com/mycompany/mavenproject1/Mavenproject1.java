ec7529973799b0800c02b0f34244b31f372603c3
Jayant <jmangla@my.centennialcollege.ca>
2025-02-24 13:23:13 +0300

    maven project

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Youngpuffy
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to COMP367");
+    }
+}
